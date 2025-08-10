b8cf113b7ae873bc5011e456fef06f26211e19a6
FadhlanSutendi <ffarizansutendi@gmail.com>
2025-08-10 12:10:43 +0700

    api

--- lib/core/api/app_api.dart
+++ lib/core/api/app_api.dart
@@ -9,7 +9,7 @@ class AppApi {
 
   /// LOGIN METHOD
   static Future<Map<String, dynamic>?> login(String username, String password) async {
-    const url = 'https://c4b2bb54f268.ngrok-free.app/api/login';
+    const url = 'https://fc2b4b2855e7.ngrok-free.app/api/login';
     try {
       final response = await _dio.post(
         url,
@@ -31,7 +31,7 @@ class AppApi {
 
   /// FETCH UNIT ITEM METHOD (dengan http package)+++++++++++++
   static Future<Map<String, dynamic>?> fetchUnitItem(String barcode, {required String token}) async {
-    final url = Uri.parse('https://c4b2bb54f268.ngrok-free.app/api/unit-items?code_unit=$barcode');
+    final url = Uri.parse('https://fc2b4b2855e7.ngrok-free.app/api/unit-items?code_unit=$barcode');
     try {
       final response = await http.get(
         url,
@@ -55,7 +55,7 @@ class AppApi {
 
   /// FETCH UNIT LOAN CHECK (for scan barcode student)
   static Future<Map<String, dynamic>?> fetchUnitLoanCheck(String codeUnit, {required String token}) async {
-    final url = Uri.parse('https://c4b2bb54f268.ngrok-free.app/api/unit-loan/check');
+    final url = Uri.parse('https://fc2b4b2855e7.ngrok-free.app/api/unit-loan/check');
     try {
       print('fetchUnitLoanCheck: token=$token, code_unit=$codeUnit'); // debug
       final response = await http.post(
@@ -82,7 +82,7 @@ class AppApi {
 
   /// FETCH STUDENT/TEACHER DATA
   static Future<Map<String, dynamic>?> fetchPerson(String id, {required String type, required String token}) async {
-    final url = Uri.parse('https://c4b2bb54f268.ngrok-free.app/api/$type/$id');
+    final url = Uri.parse('https://fc2b4b2855e7.ngrok-free.app/api/$type/$id');
     try {
       final response = await http.get(
         url,
@@ -105,7 +105,7 @@ class AppApi {
 
   /// POST UNIT LOAN
   static Future<Map<String, dynamic>?> postUnitLoan(Map<String, dynamic> data, {required String token}) async {
-    final url = Uri.parse('https://c4b2bb54f268.ngrok-free.app/api/unit-loan');
+    final url = Uri.parse('https://fc2b4b2855e7.ngrok-free.app/api/unit-loan');
     var request = http.MultipartRequest('POST', url);
     request.headers['Authorization'] = 'Bearer $token';
     data.forEach((key, value) {
