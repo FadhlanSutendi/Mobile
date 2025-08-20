70aee593c5f2588491a1268ba55c0357a4b1fe85
raflijulian12 <raflijulian356@gmail.com>
2025-08-20 20:35:30 +0700

    home

--- lib/presentation/home/controller/home_controller.dart
+++ lib/presentation/home/controller/home_controller.dart
@@ -6,17 +6,21 @@ class HomeController extends GetxController {
   var isLoadingCard = false.obs;
   var activityList = [].obs;
   var isLoadingActivity = false.obs;
+  var loanReportData = <String, int>{}.obs;
+  var isLoadingLoanReport = false.obs;
 
   @override
   void onInit() {
     super.onInit();
     fetchCardData();
     fetchLatestActivity();
+    fetchLoanReport(from: '2024', to: '2025');
   }
 
   Future<void> fetchCardData() async {
     isLoadingCard.value = true;
     final result = await AppApi.fetchDashboardCard();
+    print('fetchCardData result: $result');
     cardData.value = result?['data'] ?? {};
     isLoadingCard.value = false;
   }
@@ -24,8 +28,17 @@ class HomeController extends GetxController {
   Future<void> fetchLatestActivity() async {
     isLoadingActivity.value = true;
     final result = await AppApi.fetchDashboardLatestActivity();
+    print('fetchLatestActivity result: $result');
     activityList.value = result?['data'] ?? [];
     isLoadingActivity.value = false;
+  }
+
+  Future<void> fetchLoanReport({required String from, required String to}) async {
+    isLoadingLoanReport.value = true;
+    final result = await AppApi.fetchLoanReport(from: from, to: to);
+    print('fetchLoanReport result: $result');
+    loanReportData.value = Map<String, int>.from(result?['data'] ?? {});
+    isLoadingLoanReport.value = false;
   }
 }
 
